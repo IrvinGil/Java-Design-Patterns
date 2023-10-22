0e1a2e07a4699a646d59e2a866e7985a0fd15f20
IrvinGil <irvin.mercado@synacy.com>
2023-10-22 15:27:42 +0800

    :recycle: refactor: update class name

--- src/main/java/com/designpatterns/structural/facadepattern/FacadePatternClientDemoMain.java
+++ src/main/java/com/designpatterns/structural/facadepattern/FacadePatternClientDemoMain.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class FacadePatternClient {
+public class FacadePatternClientDemoMain {
 
     private static int choice;
 
